05f14602df027c1f0cfe5a0d1593adb32d6a0ea9
jc00ke <jesse@jc00ke.com>
2015-08-31 15:46:58 -0700

    Update to omf

--- config.fish
+++ config.fish
@@ -1,18 +1,20 @@
 # Path to your oh-my-fish.
+set -g OMF_PATH $HOME/.local/share/omf
+
+# Path to your oh-my-fish configuration.
+set -g OMF_CONFIG $HOME/.config/omf
+
+# Path to here
 set fish_path $HOME/.oh-my-fish
 set fish_config $HOME/.config/fish
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-#set fish_plugins autojump bundler git rails node gem rake
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+### Configuration required to load oh-my-fish ###
+# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
+# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
+# to create a custom plugin instead.
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-Theme "agnoster"
+source $OMF_PATH/init.fish
 
 # Load aliases
 . $fish_config/aliases.fish
@@ -33,7 +35,7 @@ set -x PATH $PATH /usr/local/heroku/bin
 . /usr/local/share/chruby/chruby.fish
 . /usr/local/share/chruby/auto.fish
 
-chruby 2.2.2
+chruby 2.2.3
 
 if test -f ~/.ssh/private_tokens
   . ~/.ssh/private_tokens.fish
@@ -44,4 +46,3 @@ if test -d ~/bin
 end
 
 eval (direnv hook fish)
-
