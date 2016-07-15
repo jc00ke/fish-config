8e138f9ce82a3c01470d629ac795b7ab428f1aaf
jc00ke <jesse@jc00ke.com>
2016-07-15 13:30:03 -0700

    Switch to Google terminal color theme

--- functions/toggle_shell_theme.fish
+++ functions/toggle_shell_theme.fish
@@ -3,5 +3,5 @@ function toggle_shell_theme
   if test -n "$argv" -a "$argv" != "$variant"
     set_theme_variant "$argv"
   end
-  eval sh $HOME/.config/base16-shell/base16-solarized."$argv".sh
+  eval sh $HOME/.config/base16-shell/base16-google."$argv".sh
 end
