8bde8ec6afc2f1b9733fd2f02eaf631d047ac2ba
Jesse Cooke <jesse@relativepath.io>
2020-01-23 21:41:36 -0800

    Fix abbreviations

--- config.fish
+++ config.fish
@@ -63,46 +63,44 @@ end
   #__ssh_agent_start
 #end
 
-# Aliases
-# =======
-alias git="hub"
-
 # Abbreviations
 # =============
 # Clean abbreviations
 set -e fish_user_abbreviations
 
-abbr -a free "free -m"
-abbr -a l "ls -la"
-abbr -a lv "nvim -R"
-abbr -a e nvim
-abbr -a es "nvim -S"
 abbr -a .. "cd .."
 abbr -a ... "cd ../.."
-abbr -a cdr "cd .. and cd -"
-abbr -a xclipx "xclip -selection clipboard"
-abbr -a git "hub"
-abbr -a gst "git status --ignore-submodules=dirty"
-abbr -a gd "git diff"
-abbr -a gdt "git difftool"
-abbr -a gc "git commit -v"
-abbr -a gca "git commit -v -a"
-abbr -a gb "git branch"
-abbr -a gba "git branch -a"
-abbr -a gm "git merge"
-abbr -a gmv "git mv"
-abbr -a ga "git add"
-abbr -a gap "git add -p"
-abbr -a gco "git checkout"
-abbr -a gsr "git svn rebase"
-abbr -a gsd "git svn dcommit"
-abbr -a gg "git log --pretty=oneline --abbrev-commit --branches=* --graph --decorate --color"
-abbr -a gfa "git commit --amend --reset-author -CHEAD"
-abbr -a conflicts "git ls-files --unmerged | cut -f2 | uniq"
-abbr -a gfp "git fetch --prune"
 abbr -a b "bundle exec"
 abbr -a cat "bat"
+abbr -a cdr "cd .. and cd -"
+abbr -a conflicts "git ls-files --unmerged | cut -f2 | uniq"
 abbr -a dc "docker-compose"
+abbr -a e nvim
+abbr -a es "nvim -S"
+abbr -a fd "fdfind"
+abbr -a free "free -m"
+abbr -a ga "git add"
+abbr -a gap "git add -p"
+abbr -a gb "git branch"
+abbr -a gba "git branch -a"
+abbr -a gc "git commit -v"
+abbr -a gca "git commit -v -a"
+abbr -a gco "git checkout"
+abbr -a gd "git diff"
+abbr -a gdt "git difftool"
+abbr -a gfa "git commit --amend --reset-author -CHEAD"
+abbr -a gfp "git fetch --prune"
+abbr -a gg "git log --pretty=oneline --abbrev-commit --branches=* --graph --decorate --color"
+abbr -a gm "git merge"
+abbr -a gmv "git mv"
+abbr -a gsd "git svn dcommit"
+abbr -a gsr "git svn rebase"
+abbr -a gst "git status --ignore-submodules=dirty"
+abbr -a hh "heroku"
+abbr -a l "ls -la"
+abbr -a lv "nvim -R"
+abbr -a xclipx "xclip -selection clipboard"
+abbr -a xx "exit"
 
 function oops
   eval command sudo $history[1]
